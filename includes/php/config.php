e92499aca363a1684bc1cf7e16562d7d99f5b9d8
TimboKZ <tima_kuzh@inbox.ru>
2016-05-14 14:05:56 +0100

    Tweaked base_url

--- includes/php/config.php
+++ includes/php/config.php
@@ -2,5 +2,5 @@
 header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set('Europe/London');
 
-$GLOBALS['base_url'] = '/';
+$GLOBALS['base_url'] = '';
 $GLOBALS['version'] = 'v0.4 Beta';
