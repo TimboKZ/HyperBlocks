7517fcf004ecaa7cf5c612440d4e3672a0c24b38
TimboKZ <tima_kuzh@inbox.ru>
2016-05-14 14:01:06 +0100

    Removed unnecessary files, tweaked base_url

--- includes/php/config.php
+++ includes/php/config.php
@@ -2,16 +2,5 @@
 header('Content-Type: text/html; charset=utf-8');
 date_default_timezone_set('Europe/London');
 
-$GLOBALS['base_url'] = 'http://timbo.kz/reddit/HyperBlocks/';
+$GLOBALS['base_url'] = '/';
 $GLOBALS['version'] = 'v0.4 Beta';
-
-$GLOBALS['mysql_user'] = 'root';
-$GLOBALS['mysql_password'] = '';
-$GLOBALS['mysql_db'] = 'hyperblocks';
-
-
-//require_once 'meekrodb.2.3.class.php';
-//DB::$user = $GLOBALS['mysql_user'];
-//DB::$password = $GLOBALS['mysql_password'];
-//DB::$dbName = $GLOBALS['mysql_db'];
-//DB::debugMode();
